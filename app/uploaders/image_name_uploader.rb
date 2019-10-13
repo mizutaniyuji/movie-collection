aa4adde09367798eb68f4be7bfa999ef288895d9
yuji.mizutani <yuzi0x14@gmail.com>
2019-10-13 13:23:56 +0000

    for heroku

--- app/uploaders/image_name_uploader.rb
+++ app/uploaders/image_name_uploader.rb
@@ -1,30 +1,28 @@
 class ImageNameUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
   
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-  else
-    include CarrierWave::MiniMagick
-    storage :file
-  end
+  # if Rails.env.production?
+  #   include Cloudinary::CarrierWave
+  # else
+  #   include CarrierWave::MiniMagick
+  #   storage :file
+  # end
 
-  # Choose what kind of storage to use for this uploader:
-    storage :file
-  # storage :fog
+  # # Choose what kind of storage to use for this uploader:
+  #   storage :file
+  # # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # # Override the directory where uploaded files will be stored.
+  # # This is a sensible default for uploaders that are meant to be mounted:
 
-  process resize_to_fit: [500, 500]
+  # # process resize_to_fit: [500, 500]
   
-  def public_id
-    model.id
-  end
+  # def public_id
+  #   model.id
+  # end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
